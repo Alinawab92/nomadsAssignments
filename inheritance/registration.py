f434adf8cd084903613369593e15ad2b8d57435f
Ali Nawab Haider <halihaider43@gmail.com>
2024-09-10 17:12:46 +0500

    changes updated

--- inheritance/registration.py
+++ inheritance/registration.py
@@ -13,7 +13,3 @@ class Registration:
     def get_details(self): 
         return f"Check IN: {self.time_in}, Time Out: {self.time_out}, Car Tracker: {self.car_track}"
     
-# Example usage
-c1 = Registration("3:00", "5:00", "2nd time comes")
-c1.update("10:00", "2:00", "4th time comes") 
-print(c1.get_details())
